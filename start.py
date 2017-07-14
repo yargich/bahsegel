9a97fb838f26e22080fc09c511cd3755ffd22dab
Oleksii Havrylov <oleksii.havrylov@gamingtec.com>
2017-07-14 11:58:03 +0300

    Corrected description of tests. Fixed error with imports.

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium import webdriver
+import pdb
 '''
 https://selenium2.ru/docs/selenium-grid.h
 Test functionality of site	
@@ -25,22 +25,23 @@ logout
 URL = "https://tst.bahsegel.info"
 class Bahsegel:
     def __init__(self):
+        '''Open site which based on URL parameter'''
         self.driver = webdriver.Firefox()
         driver = self.driver
         self.driver.implicitly_wait(30)
         self.base_url = URL
-
-        driver.get(self.base_url + "/tr")
+        driver.get(self.base_url + '/tr')
 
 
     def login(self):
+        '''Enter login and password and provides login of user'''
+        driver = self.driver
         username = "bahsegel_26"
         passw = "qwerty26"
-
         self.driver.find_element_by_id("login-Button").click()
-        # driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").clear()
         self.driver.find_element_by_id("username").send_keys(username)
-        # driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").clear()
         self.driver.find_element_by_id("password").send_keys(passw)
         self.driver.find_element_by_id("loginBtnSubmit").click()
 
@@ -55,9 +56,10 @@ class Bahsegel:
         driver = self.driver
         frame = driver.find_element_by_xpath('//*[@id="sport_iframe_1"]')
         driver.switch_to.frame(frame)
+        pdb.set_trace()
         driver.find_element_by_xpath('//*[@id="champFav15829"]/span').click()
         # driver.find_element_by_xpath("//div[@id='mCSB_8_container']/div/div[2]/div[2]/a/div[2]").click()
-        driver.find_element_by_xpath("//*[@id='mCSB_7_container']/div/div[2]/div[2]/a[1]/div[2]").click()
+        # driver.find_element_by_xpath("//*[@id='mCSB_7_container']/div/div[2]/div[2]/a[1]/div[2]").click()
         # driver.find_element_by_id("betAmountInput").clear()
         # driver.find_element_by_id("betAmountInput").send_keys("2")
 
