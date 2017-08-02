342812bada7af317360e207a14bf1f409b9def01
goodwin <oleksii.havrylov@gamingtec.com>
2017-08-02 16:28:44 +0300

    Added forgot_password method

--- just_actions.py
+++ just_actions.py
@@ -20,7 +20,7 @@ class Action(object):
     def __init__(self):
         self.url = URLtst
         self.driver = webdriver.Chrome()
-        self.driver.set_window_size(1300, 700)
+        self.driver.set_window_size(1800, 1000)
         self.driver.get(self.url)
         self.driver.implicitly_wait(30)
 
@@ -32,8 +32,6 @@ class Action(object):
             print i
 
     def get_favicon(self):
-
-
         favicon_url= self.url+'/favicon.png'
         print favicon_url
         picture = requests.get(favicon_url, auth=('beta5', 'gamma8'))
@@ -114,7 +112,9 @@ class Action(object):
         driver.find_element_by_id('username').send_keys(name)
         driver.find_element_by_id('password').clear()
         driver.find_element_by_id('password').send_keys(passw)
-        driver.find_element_by_id('loginBtnSubmit').click()
+
+        subbutton_xpath = '//*[@id="loginBtnSubmit"]'
+        driver.find_element_by_xpath(subbutton_xpath).click()
         print "Login with users name {0} and password {1} was successfull...".format(NAME,PASSW)
         return 'login successfull'
 
@@ -158,6 +158,8 @@ class Action(object):
 
 
     def check_top_menu(self):
+
+
         driver = self.driver
         base_xpath = '//*[@id="bodyScope"]/header[1]/nav/div/ul/li'
         top_menu = driver.find_elements_by_xpath(base_xpath)
@@ -171,9 +173,16 @@ class Action(object):
             i += 1
 
         return num_of_element - 1
-    def top_menu_with_login(self,name,passw):
+    def forgot_password(self):
+        driver = self.driver
+        driver.find_element_by_id('login-Button').click()
+        forgot_password = driver.find_element_by_xpath('//*[@id="bodyScope"]/div[1]/div[3]/div/div[2]/form/div[4]/div[1]/a')
+        forgot_password.click()
+        value = forgot_password.text
+        return value
 
-        self.check_top_menu()
+
+
     def virtual_sport_click(self):
         driver = self.driver
         self.enter_to_virtual_sport()
@@ -188,9 +197,12 @@ class Action(object):
         horseracingXPATH = '//*[@id="bodyScope"]/div[1]/div/ul/li[5]/a/span'
         driver.find_element_by_xpath(horseracingXPATH).click()
 
-    def open_result_in_browser(self, file):
-        filename = "nosetests.html"
-        webbrowser.open(filename)
+    def open_result_in_browser(self, filename = "nosetests.html"):
+        '''opens results in Chrome'''
+
+        myComand = 'open -a \'Google Chrome\ '+filename
+        os.system(myComand)
+        # webbrowser.open(filename)
 
     def switch_frame(self):
         driver = self.driver
@@ -198,7 +210,7 @@ class Action(object):
         driver.switch_to.frame(my_frame)
 
     def close_driver(self):
-        self.driver.quit()
+        self.driver.close()
     def enter_registered_users(self,user_name,user_login):
         self.login(user_name,user_login)
         self.close_driver()
@@ -208,24 +220,14 @@ class Action(object):
         self.logo_url()
         self.logo_exist()
         self.open_svg_in_browser()
-    # def start_request(self):
-    #     url = self.url
-    #     payload = " "
-    #     headers = {
-    #
-    #         'login': "beta5",
-    #         'password': "gamma8",
-    #         'cache-control': "no-cache",
-    #
-    #     }
-    #     response = requests.request("GET", url, data=payload, headers=headers)
-    #     print response.text
+
 
 if __name__ == "__main__":
     test = Action()
     # test.login(NAME,PASSW)
     # test.open_svg_in_browser()
-    test.get_favicon()
+    test.forgot_password()
+    # test.get_favicon()
     # test.get_picture('favicon.png')
     # test.if_logo_are_exists()
     # test.open_svg_in_browser()
