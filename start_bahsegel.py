3e1f65c5892b58d7873eab7a7c46cf21784c0519
goodwin <oleksii.havrylov@gamingtec.com>
2017-08-03 12:01:05 +0300

    Added pre_condition, which removes odd files from working directory

--- start_bahsegel.py
+++ start_bahsegel.py
@@ -15,9 +15,20 @@ PASSW = "qwerty26"
 class Bahsegel(unittest.TestCase):
     def setUp(self):
         print 'Testing now:'
+        self.pre_condition()
+
+    def pre_condition(self):
+
+        for i in os.listdir('./'):
+            Suff = ['.png', '.html']
+            for item in Suff:
+                if i.endswith(item):
+                    os.remove(i)
+                    print i + " is deleted..."
 
     def test_favicon_exists(self):
         test = Action()
+
         test.show_name('favicon exists')
         test.get_favicon()
         test.close_driver()
@@ -43,9 +54,9 @@ class Bahsegel(unittest.TestCase):
         test.close_driver()
 
     def test_url_of_login_page(self):
-        test.show_name('Url of login page')
         '''check url on login page'''
         test = Action()
+        test.show_name('Url of login page')
         link_to_login = test.enter_to_page()
         self.assertEqual(link_to_login[0], link_to_login[1])
         test.close_driver()
