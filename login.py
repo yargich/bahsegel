9ee82a0d9d465a86e71265cfb190109405479941
goodwin <oleksii.havrylov@gamingtec.com>
2017-08-07 12:32:22 +0300

    Added tests to login.py

---
+++ login.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from just_actions import *
+
+import unittest,time,sys
+
+class Bahsegel(unittest.TestCase):
+    def setUp(self):
+        print 'Testing now...'
+
+
+    def pre_condition(self):
+        for i in os.listdir('./'):
+            Suff = ['.png','.svg']
+            for item in Suff:
+                if i.endswith(item):
+                    os.remove(i)
+
+
+
+
+    def test_04(self):
+        '''check url on login page'''
+        test = Action()
+
+        link_to_login = test.enter_to_page()
+        self.assertEqual(link_to_login[0], link_to_login[1])
+        test.close_driver()
+
+    def test_06(self):
+        '''successfully login'''
+        test = Action()
+
+        test.login(NAME,PASSW)
+        test.close_driver()
+
+
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
