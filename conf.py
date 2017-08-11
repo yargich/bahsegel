e4bfae685e615a3f1085039413fe72b8c6000cb3
goodwin <oleksii.havrylov@gamingtec.com>
2017-08-11 19:37:35 +0300

    Added conf.py with basick url and creds

--- conf.py
+++ conf.py
@@ -3,3 +3,7 @@
 BahsegelTST = 'https://tst.bahsegel.info'
 BahsegelLogin = "Bahsegel_26"
 BahsegelPassw = "qwerty26"
+
+BahsegelSTG = 'https://stg.bahsegel.info'
+BahsegelSTGLogin = BahsegelLogin
+BahsegelSTGPassw = BahsegelPassw
